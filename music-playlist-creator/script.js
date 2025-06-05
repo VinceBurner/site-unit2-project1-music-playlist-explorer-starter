9ce270fc9812d7a7293d8f04f89823bca133b589
Vincent Anim-Addo <vince477@meta.com>
2025-06-04 23:18:50 -0700

    Your commit message here

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,37 @@
+window.addEventListener("DOMContentLoaded",() => {
+    const gallery = document.getElementById("playlist-gallery");
+    const modal = document.getElementById("modal");
+    const closeButton = document.querySelector(".close-button");
+    const modalImage = document.querySelector(".modal-details img");
+    const modalTitle = document.querySelector(".modal-details h2");
+    const modalAuthor = document.querySelector(".modal-details p");
+    const songList = document.querySelector(".song-list");
 
+    let playlistData = []
+
+    fetch("data/data.json")
+    .then(response => response.json())
+    .then(data => {
+        playlistsData = data.playlists;
+        if (!data.playlists || data.playlists === 0) {
+            gallery.innerHTML = "<p>No playlists added.</p>";
+            return;
+        }
+
+        data.playlists.forEach(playlist => {
+            const card = document.createElement("div");
+            card.className = "playlist-card";
+            card.innerHTML = `
+            <img src="${playlist.playlist_art}" alt="Playlist Cover">
+            <h3>${playlist.playlist_name}</h3>
+            <p>by ${playlist.playlist_author}</p>
+            <p>❤️ ${playlist.songs.length}</p>
+            ' ;
+            gallery.appendChild(card);
+        });
+    })
+    .catch(error => {
+        gallery.innerHTML = "<p>Error loading playlists.</p>";
+        console.error("Fetch error: "error);
+    });
+});
